62f4fda8962ca827ea39f8d914a4d323e896a90d
Marina Bobrenko <m.bobrenko@gmail.com>
2023-06-17 11:55:28 +0300

    add 005 examples random

---
+++ 005examples/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Eneter name of user: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Finally,it's Masha");
+}
+else
+{
+Console.Write("Hey ");
+Console.WriteLine(username);
+}
