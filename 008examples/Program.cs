bd2d7e89cb8eaba8ead2f358dc0f31ffcdba9e56
Marina Bobrenko <m.bobrenko@gmail.com>
2023-06-19 17:34:46 +0300

    009examples added

---
+++ 008examples/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найти максимальное из 9 
+int a1 = 23;
+int b1 = 43;
+int c1 = 36;
+int a2 = 39;
+int b2 = 33;
+int c2 = 623;
+int a3 = 13;
+int b3 = 253;
+int c3 = 33;
+
+int max = a1;
+if(b1>max) max=b1;
+if(c1>max) max=c1;
+if(a2>max) max=a2;
+if(b2>max) max=b2;
+if(c2>max) max=c2;
+if(a3>max) max=a3;
+if(b3>max) max=b3;
+if(c3>max) max=c3;
+
+Console.WriteLine (max);
+
+
---
+++ 009examples/Program.cs
@@ -0,0 +1,30 @@
+﻿// Найти максимальное из 9
+
+
+int Max(int arg1, int arg2, int arg3);
+
+{
+int result =arg1;
+ if (arg2 > result) result = arg2;
+ if (arg3> result) result = arg3;
+    return result;
+}
+
+int a1 = 23;
+int b1 = 43;
+int c1 = 36;
+int a2 = 39;
+int b2 = 33;
+int c2 = 623;
+int a3 = 13;
+int b3 = 253;
+int c3 = 33;
+
+//int max1= Max(a1,b1,c1);
+//int max2= Max(a2,b2,c2);
+//int max3= Max(a3,b3,c3);
+//int max= Max(max1, max2, max3);
+
+int max=Max(Max(a1,b1,c1),Max(a2,b2,c2),Max(a3,b3,c3));
+
+Console.WriteLine(max);
