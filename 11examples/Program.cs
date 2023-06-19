830e21e783ef9561f1089320a000bcef86851516
Marina Bobrenko <m.bobrenko@gmail.com>
2023-06-19 18:13:59 +0300

    11examples added

---
+++ 11examples/Program.cs
@@ -0,0 +1,14 @@
+﻿        int [] Array = {11, 22, 33, 44, 44, 66, 77, 888, 9 };
+        int n = Array.Length;
+        int find = 44;
+        int index = 0;
+        while (index < n)
+        {
+            if (Array[index] == find)
+            {
+                Console.WriteLine(index);
+                break;
+            }
+            index++;
+        }
+    
